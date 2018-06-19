c437a0c55f70ccbf127429cdc0813e88c403c5f7
straccers <ps552@york.ac.uk>
2018-06-19 15:32:51 +0100

    new files added to begin dept name work
    added new ruby class department_name_normaliser.rb and new file to run rake tasks for it (department_manipulation_tasks.rake)
    these are in a very early stage of development and dont do much

--- lib/department_name_normaliser.rb
+++ lib/department_name_normaliser.rb
@@ -10,12 +10,45 @@ class DepartmentNameNormaliser
 	    puts "hi! from department normaliser"
 	end
 
+	#default output to dlib-migration-tools root dir
+	def check_single_file(filepath)
+		doc = File.open(filepath){ |f| Nokogiri::XML(f, Encoding::UTF_8.to_s)}
+		ns = doc.collect_namespaces # doesnt resolve nested namespaces, this fixes that
+		# find max dc version
+		nums = doc.xpath("//foxml:datastream[@ID='DC']/foxml:datastreamVersion/@ID",ns)
+		all = nums.to_s
+		current = all.rpartition('.').last
+		current_dc_version = 'DC.' + current
+
+		#this may get messy.
+		#in exams the dept may be under creator OR publisher (a minority)
+		#in theses the department will be the publisher - the student is the creator
+		#in undergrad papers/projects dept may be in publishe OR creator - but
+		# creator may also contain actual student names. Needs some fuzzy logic!
+		#populate this then call a dissambiguation script to remove the unwanted
+		dept = []
+		doc.xpath("//foxml:datastream[@ID='DC']/foxml:datastreamVersion[@ID='#{current_dc_version}']/foxml:xmlContent/oai_dc:dc/dc:publisher/text()",ns).each do |s|
+			dept.push(s.to_s)
+			puts "still checking..."
+		end
+		doc.xpath("//foxml:datastream[@ID='DC']/foxml:datastreamVersion[@ID='#{current_dc_version}']/foxml:xmlContent/oai_dc:dc/dc:creator/text()",ns).each do |s|
+			dept.push(s.to_s)
+			puts "still checking..."
+		end
+
+		dept.each do |d|
+			return_values = []
+			puts d.to_s
+		#	return return_values
+		end
+	end
+
 
 	#default action if ruby date_normaliser.rb called from lib folder
 	if __FILE__==$0
 		d = DepartmentNameNormaliser.new
 		puts "please type a department name"
 		name = gets
-		d.test_normalisation(name)
+		puts "you typed " + name +" for me"
 	end
 end
---
+++ lib/tasks/department_manipulation_tasks.rake
@@ -0,0 +1,30 @@
+namespace :department_manipulation_tasks do
+desc "department name normalisation tools"
+
+require_relative '../../lib/department_name_normaliser.rb'
+
+	task :greet do
+		puts "greetings from the new department manipulation tasks"
+	end
+
+	task :say_hi do
+		d = DepartmentNameNormaliser.new
+		d.say_hi
+	end
+
+	task :test, [:filepath] do |t, args|
+		puts args[:filepath]
+		param = args[:filepath]
+		d = DepartmentNameNormaliser.new
+		d.say_hi
+		d.check_single_file(args[:filepath])
+	end
+
+	#task :check_all_date_formats, [:directory_path,:info_level] do |t, args|
+	#	puts args[:directory_path]
+	#	args.with_defaults(:info_level => 'less')
+	#	d = DateNormaliser.new
+	#	d.check_all_date_formats(args[:directory_path],args[:info_level])
+	#end
+
+end
