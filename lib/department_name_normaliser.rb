5145fe2c5245fcfaa324ca9c29f16c7b6b548114
straccers <ps552@york.ac.uk>
2018-06-21 10:47:45 +0100

    refactoring

--- lib/department_name_normaliser.rb
+++ lib/department_name_normaliser.rb
@@ -2,25 +2,30 @@
 # encoding: UTF-8
 require 'nokogiri'
 class DepartmentNameNormaliser
+
+
+
 	def initialize
 	   puts "initialising DepartmentNameNormaliser"
+		 @unique_initial_values = []
+		 @unique_dept = []
 	end
 
 	def say_hi
 	    puts "hi! from department normaliser"
+			animal = "cat"
+			unless animal == "dog"
+				puts "not a cat"
+			end
 	end
 
 
 	def check_folder(folderpath)
- 		#outfile = File.open("corrected_dates_list.txt", "a")
-		unique_dept = []
-		unique_initial_values = [] #for debug
  		directory_to_check = folderpath.strip
  		Dir.foreach(directory_to_check.strip)do |item|
  			next if item == '.' or item == '..'
- 			#filepath = "../data" + "/" + item
  			filepath = directory_to_check + "/" + item
-			check_single_file(filepath, unique_dept, unique_initial_values)
+			check_single_file(filepath)
  			#normalised_date = check_single_file(filepath)
  			#returned_values = check_single_file(filepath)
  			#date_in = returned_values[0]
@@ -35,21 +40,20 @@ class DepartmentNameNormaliser
  		#		end
  		#	end
  		end
-		puts "size of unique_dept " + unique_dept.size.to_s
 		outfile = File.open("unique_department_names.txt", "a")
-		unique_dept.each do |d|
+		@unique_dept.each do |d|
 			outfile.puts(d.to_s)
 		end
 
 		outfile2 = File.open("unique_initial_values.txt", "a")
-		unique_initial_values.each do |d|
+		@unique_initial_values.each do |d|
 			outfile2.puts(d.to_s)
 		end
  	end
 
 	#we now need to manipulate the data to remove the elements we dont want. do this in a separate method.
 	#default output to dlib-migration-tools root dir
-	def check_single_file(filepath, unique_dept, unique_initial_values)
+	def check_single_file(filepath)
 		doc = File.open(filepath){ |f| Nokogiri::XML(f, Encoding::UTF_8.to_s)}
 		ns = doc.collect_namespaces # doesnt resolve nested namespaces, this fixes that
 		# find max dc version
@@ -65,40 +69,32 @@ class DepartmentNameNormaliser
 		#look in creators first
 		doc.xpath("//foxml:datastream[@ID='DC']/foxml:datastreamVersion[@ID='#{current_dc_version}']/foxml:xmlContent/oai_dc:dc/dc:creator/text()",ns).each do |s|
       possible_value = s.to_s
-			if unique_initial_values.include? (possible_value)
-			 #do nothing
-		 	else
-			 unique_initial_values.push(possible_value)
+			unless @unique_initial_values.include? (possible_value)
+			 	@unique_initial_values.push(possible_value)
 		 	end
 			filter_result = filter_department_values(possible_value)
 			if filter_result != "false"
 				dept.push(s.to_s)
-				puts "dc:publisher 	" + possible_value
 			end
 		end
-		#didnt find a value looking like a department or institution in publisher, so check publishers
+		#didnt find a value looking like a department or institution in creators, so check publishers
 		if dept.size < 1
 			doc.xpath("//foxml:datastream[@ID='DC']/foxml:datastreamVersion[@ID='#{current_dc_version}']/foxml:xmlContent/oai_dc:dc/dc:publisher/text()",ns).each do |s|
 				possible_value = s.to_s
-				if unique_initial_values.include? (possible_value)
-				 #do nothing
-			 	else
-				 unique_initial_values.push(possible_value)
+				unless  @unique_initial_values.include? (possible_value)
+				 	@unique_initial_values.push(possible_value)
 			 	end
 				filter_result = filter_department_values(possible_value)
 				if filter_result != "false"
 					dept.push(s.to_s)
-					puts "dc:creator 	" + possible_value
 				end
 			end
 		end
 		# list all unique dept values found for debugging
 		dept.each do |d|
 		 d = d.to_s
-			if unique_dept.include? (d)
-			 #do nothing
-		 	else
-			 unique_dept.push(d)
+			unless @unique_dept.include? (d)
+			 	@unique_dept.push(d)
 		 	end
 		end
 	end
@@ -108,7 +104,7 @@ class DepartmentNameNormaliser
 	#this just extracts the value of the initial string - not yet stanardised
 	def filter_department_values(value)
 		term_to_filter = value.downcase
-		filter_words = ["university","dept","department","school","studies"]
+		filter_words = ["university","dept","department","school","studies","language","unit","edexel","education","aqa","ocr"]
 		filter_words.each do |w|
 			if term_to_filter.include? w
 				return term_to_filter
