ff09c3cf25dfa129b993641ed688fe78e933cc46
straccers <ps552@york.ac.uk>
2018-07-09 17:19:18 +0100

    get current dc version
    establish which version of dc is current, and also make the command line 
    call example generic

--- lib/dublin_core_elements_extractor.rb
+++ lib/dublin_core_elements_extractor.rb
@@ -12,5 +12,10 @@ class DublinCoreElementsExtractor
     ns = doc.collect_namespaces # doesnt resolve nested namespaces, this fixes that
     pid = doc.xpath("//foxml:digitalObject/@PID",ns).to_s
     puts "read the pid:"+ pid.to_s
+    nums = doc.xpath("//foxml:datastream[@ID='DC']/foxml:datastreamVersion/@ID",ns)
+		all = nums.to_s
+		current = all.rpartition('.').last
+		current_dc_version = 'DC.' + current
+    puts "current dc version is " + current_dc_version
   end
 end
