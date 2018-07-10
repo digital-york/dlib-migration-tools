6f5d0b7a00b0b8f4c810817b1fddac345e0a8c91
straccers <ps552@york.ac.uk>
2018-07-10 14:29:57 +0100

    extract remaining key elements using generic extraction methods

--- lib/dublin_core_elements_extractor.rb
+++ lib/dublin_core_elements_extractor.rb
@@ -11,67 +11,25 @@ class DublinCoreElementsExtractor
 	end
 
   #this will do the meat of extracting the metadata values and putting them into a hash
-  def extract_key_metadata(doc)
-    #key_metadata = Hash.new
-    ns = doc.collect_namespaces #  nokogiri out of box doesnt resolve nested namespaces, this fixes that
-    @ns = doc.collect_namespaces #  nokogiri out of box doesnt resolve nested namespaces, this fixes that
-    pid = doc.xpath("//foxml:digitalObject/@PID",ns).to_s
-    @key_metadata[:pid] =pid
+  def extract_key_metadata
+    @ns = @doc.collect_namespaces #  nokogiri out of box doesnt resolve nested namespaces, this fixes that
+    pid = @doc.xpath("//foxml:digitalObject/@PID",@ns).to_s #unique path structure
+    @key_metadata[:pid] = pid
     #estabish current dc version to use before extracting any further values
-    nums = doc.xpath("//foxml:datastream[@ID='DC']/foxml:datastreamVersion/@ID",ns)
+    nums = @doc.xpath("//foxml:datastream[@ID='DC']/foxml:datastreamVersion/@ID",@ns) #unique path structure
 		all = nums.to_s
 		current = all.rpartition('.').last
-		current_dc_version = 'DC.' + current
     @current_dc_version = 'DC.' + current
     #extract rest of key metadata
-
-    #title (should always be present, single value)
-    title = doc.xpath("//foxml:datastream[@ID='DC']/foxml:datastreamVersion[@ID='#{current_dc_version}']/foxml:xmlContent/oai_dc:dc/dc:title/text()",ns).to_s
-    @key_metadata[:title] = title
-
-    #creators (multivalue or absent)
-    #creators = []
-	#	doc.xpath("//foxml:datastream[@ID='DC']/foxml:datastreamVersion[@ID='#{current_dc_version}']/foxml:xmlContent/oai_dc:dc/dc:creator/text()",ns).each do |s|
-    #    creators.push(s.to_s)
-	#	end
-  #  if creators.size > 0
-  #    @key_metadata[:creators] = creators
-  #  end
-    extract_multivalued_element("creator")
-
-    #publishers (multivalue or absent)
-    publishers = []
-		doc.xpath("//foxml:datastream[@ID='DC']/foxml:datastreamVersion[@ID='#{@current_dc_version}']/foxml:xmlContent/oai_dc:dc/dc:publisher/text()",ns).each do |s|
-        publishers.push(s.to_s)
-		end
-    if publishers.size > 0
-      @key_metadata[:publishers] = publishers
-    end
-
-    #subject (multivalue or absent)
-    subjects = []
-		doc.xpath("//foxml:datastream[@ID='DC']/foxml:datastreamVersion[@ID='#{current_dc_version}']/foxml:xmlContent/oai_dc:dc/dc:subject/text()",ns).each do |s|
-        subjects.push(s.to_s)
-		end
-    if subjects.size > 0
-      @key_metadata[:subjects] = subjects
-    end
-
-    #rights (multivalue)
-    rights = []
-    doc.xpath("//foxml:datastream[@ID='DC']/foxml:datastreamVersion[@ID='#{current_dc_version}']/foxml:xmlContent/oai_dc:dc/dc:rights/text()",ns).each do |s|
-        rights.push(s.to_s)
-		end
-    if rights.size > 0
-      @key_metadata[:rights] = rights
-    end
-
-    #date (single value, possibly absent)
-    date = doc.xpath("//foxml:datastream[@ID='DC']/foxml:datastreamVersion[@ID='#{current_dc_version}']/foxml:xmlContent/oai_dc:dc/dc:date/text()",ns).to_s
-    if date.length > 0
-      @key_metadata[:date] = date
-    end
-
+    extract_single_valued_element("title")
+    extract_multivalued_element("creator") # may be department or personal name
+    extract_multivalued_element("publisher") # alternate possible lcoation of department
+    extract_multivalued_element("subject")
+    extract_multivalued_element("description")
+    extract_single_valued_element("date")
+    extract_multivalued_element("type") # may include model types, resource types (eg Exam paper) exam levels, Qualification names
+    extract_multivalued_element("rights") # may include copyright holder, rights statement, rights url
+    extract_multivalued_element("identifier") # may include pid, which we dont need twice,  but also module code    
     return @key_metadata
   end
 
@@ -79,13 +37,26 @@ class DublinCoreElementsExtractor
   def extract_multivalued_element(element_name)
     element_array = []
     path = "//foxml:datastream[@ID='DC']/foxml:datastreamVersion[@ID='#{@current_dc_version}']/foxml:xmlContent/oai_dc:dc/dc:" + element_name + "/text()"
-    @doc.xpath(path,@ns).each do |s|  element_array.push(s.to_s)
-        puts "found this " + s.to_s
+    @doc.xpath(path,@ns).each do |s|
+      element_array.push(s.to_s)
+      puts "found this " + s.to_s
 		end
     if element_array.size > 0
       keyname = element_name + "s"
       keyname = keyname.to_sym
       @key_metadata[keyname] = element_array
+    end
+  end
+
+  #generic method to return single value where element is a simple text value which may occur  0:1 times in a single dc version
+  def extract_single_valued_element(element_name)
+    element = ""
+    path = "//foxml:datastream[@ID='DC']/foxml:datastreamVersion[@ID='#{@current_dc_version}']/foxml:xmlContent/oai_dc:dc/dc:" + element_name + "/text()"
+    element = @doc.xpath(path,@ns).to_s
+    puts "found this " +element
+    if element.length > 0
+      keyname = element_name.to_sym
+      @key_metadata[keyname] = element
     end
   end
 
