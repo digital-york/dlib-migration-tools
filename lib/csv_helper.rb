90f47e155ce3f4fe2785fa018e60e03747c63340
straccers <ps552@york.ac.uk>
2018-07-10 10:39:19 +0100

    pid, creator, publisher values  extracted
    creator and publisher values are multivalued arrays

--- lib/csv_helper.rb
+++ lib/csv_helper.rb
@@ -10,12 +10,20 @@ class CsvHelper
 
   def collect_dc_metadata(filename)
     #open a foxml file and pass to ExtractDublinCoreMetadata
-    puts "this will open a foxml file and return a hash of key element names and values"
-    #doc = File.open(filename, "a")
       doc = File.open("../small_data/york_666.xml"){ |f| Nokogiri::XML(f, Encoding::UTF_8.to_s)}
     dc_metadata_extractor = DublinCoreElementsExtractor.new
-    dc_metadata_extractor.extract_key_metadata(doc)
-    #pass returned hash to csv creation
+    values_hash = dc_metadata_extractor.extract_key_metadata(doc)
+    keys = values_hash.keys
+    keys.each do |k|
+      puts "found key " + k.to_s
+    end
+    # get value back from hash
+    creators = values_hash.fetch(:creators)
+    creators.each do |c|
+      puts "creator was " + c.to_s
+    end
+
+    #to pass returned hash to csv creation
   end
 
   def create_csv(dc_elements_hash,output_location)
--- lib/dublin_core_elements_extractor.rb
+++ lib/dublin_core_elements_extractor.rb
@@ -3,19 +3,40 @@ require 'nokogiri'
 
 class DublinCoreElementsExtractor
 
+  #this will do the meat of extracting the metadata values and putting them into a hash
   def extract_key_metadata(doc)
-    #this will do the meat of extracting the metadata values and putting them into a hash
-    #first confirm we are indeed opening the file by getting some element back
-    #this is for debugging only as did not recognise the collect_namespaces method
-    #from filehandle passed in as parameter.so need to find how this can be done.
-    #doc = File.open("../small_data/york_666.xml"){ |f| Nokogiri::XML(f, Encoding::UTF_8.to_s)}
-    ns = doc.collect_namespaces # doesnt resolve nested namespaces, this fixes that
-    pid = doc.xpath("//foxml:digitalObject/@PID",ns).to_s
-    puts "read the pid:"+ pid.to_s
+    key_metadata = Hash.new
+    ns = doc.collect_namespaces #  nokogiri out of box doesnt resolve nested namespaces, this fixes that
+    v_pid = doc.xpath("//foxml:digitalObject/@PID",ns).to_s
+    key_metadata[:pid] = v_pid
+    #estabish current dc version to use before extracting any further values
     nums = doc.xpath("//foxml:datastream[@ID='DC']/foxml:datastreamVersion/@ID",ns)
 		all = nums.to_s
 		current = all.rpartition('.').last
 		current_dc_version = 'DC.' + current
-    puts "current dc version is " + current_dc_version
+    #extract rest of key metadata
+
+    #creators
+    creators = []
+		doc.xpath("//foxml:datastream[@ID='DC']/foxml:datastreamVersion[@ID='#{current_dc_version}']/foxml:xmlContent/oai_dc:dc/dc:creator/text()",ns).each do |s|
+        creators.push(s.to_s)
+		end
+    if creators.size > 0
+      key_metadata[:creators] = creators
+    end
+
+    #publishers
+    publishers = []
+		doc.xpath("//foxml:datastream[@ID='DC']/foxml:datastreamVersion[@ID='#{current_dc_version}']/foxml:xmlContent/oai_dc:dc/dc:publisher/text()",ns).each do |s|
+        publishers.push(s.to_s)
+		end
+    if publishers.size > 0
+      key_metadata[:publishers] = publishers
+    end
+
+
+    return key_metadata
+
   end
+
 end
