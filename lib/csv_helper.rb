179f9d54747a160c4468887a7ecf0bb4467214f5
straccers <ps552@york.ac.uk>
2018-07-11 11:47:11 +0100

    Add key metadata from RELS-EXT

--- lib/csv_helper.rb
+++ lib/csv_helper.rb
@@ -1,6 +1,7 @@
 #!/usr/bin/env ruby
 require 'nokogiri'
 require_relative 'dublin_core_elements_extractor.rb'
+require_relative 'rels_ext_elements_extractor.rb'
 
 class CsvHelper
 
@@ -8,19 +9,15 @@ class CsvHelper
 		@output_location = output_location
 	end
 
-  def collect_dc_metadata(filename)
+  def collect_metadata(filename)
     #open a foxml file and pass to ExtractDublinCoreMetadata
     doc = File.open(filename){ |f| Nokogiri::XML(f, Encoding::UTF_8.to_s)}
     dc_metadata_extractor = DublinCoreElementsExtractor.new(doc)
-    values_hash = dc_metadata_extractor.extract_key_metadata
-    keys = values_hash.keys
-    # get value back from hash
-    creators = values_hash.fetch(:creators)
-    creators.each do |c|
-      puts "creator was " + c.to_s
-    end
+    dc_values_hash = dc_metadata_extractor.extract_key_metadata
+    rels_ext_metadata_extractor = RelsExtElementsExtractor.new(doc)
+    rels_ext_values_hash = rels_ext_metadata_extractor.extract_key_metadata
     # pass returned hash to csv creation
-    create_csv(values_hash)
+    create_csv(dc_values_hash,rels_ext_values_hash)
   end
 
   # TODO: elements also need extraction from rels-ext (parent collection membership)
@@ -29,52 +26,27 @@ class CsvHelper
   # Possibly also ACL data to get access permissions
 
  #This method will create a single csv file from a single foxml file
-  def create_csv(dc_hash)
+  def create_csv(dc_hash,rels_ext_hash)
     pid = dc_hash.fetch(:pid)
     outfile_name = pid.sub ':', '_'
     outfile_path = @output_location + "/" + outfile_name + ".csv"
     CSV.open(outfile_path, "wb") do |csv|
-      csv_row = get_csv_row(dc_hash)
+      csv_row = get_csv_row(dc_hash,rels_ext_hash)
 			csv << csv_row
 		end
   end
 
-  def get_csv_row(dc_hash)
+  def get_csv_row(dc_hash, rels_ext_hash)
     csv_row = []
     dc_hash.each do |key,value|
       item = "#{key}::#{value}" #use :: as separator because pid uses colon, as may parts of some values
-      csv_row.push(item)
+      csv_row.push(item) #adds another set of quotes but it appears this is valid https://stackoverflow.com/questions/40166811/writing-to-csv-is-adding-quotes
+    end
+    rels_ext_hash.each do |key,value|
+      item = "#{key}::#{value}" #use :: as separator because pid uses colon, as may parts of some values
+      csv_row.push(item) #adds another set of quotes but it appears this is valid https://stackoverflow.com/questions/40166811/writing-to-csv-is-adding-quotes
     end
     return csv_row
   end
-
-=begin
-  def get_csv_row(filepath)
-		filepath = filepath.strip
-		date_normaliser = DateNormaliser.new
-		department_name_normaliser = DepartmentNameNormaliser.new
-		standard_date_result = date_normaliser.check_single_file(filepath)
-		standard_date = standard_date_result[1]
-		department_names_result = department_name_normaliser.check_single_file(filepath)
-	    pid = department_names_result[0].pid
-		csv_row = []
-		csv_row.push(pid)
-		if standard_date == "no match"
-			msg = pid + " date error"
-			log(msg)
-		else
-			csv_row.push(standard_date)
-		end
-		department_names_result.each do |d|
-			if d.standard_name == "no value found" or d.standard_name == "not found" or d.standard_name == "needs prior edit" or d.standard_name.length == 0
-			    msg = pid + " department name error " + d.standard_name
-				log(msg)
-			else
-				csv_row.push(d.standard_name)
-			end
-		end
-			return csv_row
-	end
-=end
 
 end
--- lib/tasks/dc_metadata_extraction_tasks.rake
+++ lib/tasks/dc_metadata_extraction_tasks.rake
@@ -1,4 +1,4 @@
-namespace :dc_metadata_extraction_tasks do
+namespace :metadata_extraction_tasks do
 desc "extract key dublin core metadata elements from foxml files"
 
 require_relative '../../lib/dublin_core_elements_extractor.rb'
@@ -8,11 +8,11 @@ require_relative '../../lib/csv_helper.rb'
     puts "greetings from the new dc_metadata_extraction_tasks"
   end
 
-  #rake dc_metadata_extraction_tasks:run_dc_metadata_collection[<"/path/to/file">,<"/path_to_output_location">]
-	task :run_dc_metadata_collection, [:foxml_name,:output_location] do |t, args|
+  #rake metadata_extraction_tasks:run_metadata_collection[<"/path/to/file">,<"/path_to_output_location">]
+	task :run_metadata_collection, [:foxml_name,:output_location] do |t, args|
 		args.with_defaults(:output_location => 'tmp')
 		ch = CsvHelper.new(args[:output_location])
-    ch.collect_dc_metadata(args[:foxml_name])
+    ch.collect_metadata(args[:foxml_name])
 	end
 
 end
