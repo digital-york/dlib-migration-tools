1ee9b41bc0487d05964539519191aaadd27fb5bf
straccers <ps552@york.ac.uk>
2018-08-02 10:44:02 +0100

    get only current versions of content locations
    binary content datastreams may also be versioned so only extract current 
    versions

--- lib/content_location_extractor.rb
+++ lib/content_location_extractor.rb
@@ -6,7 +6,6 @@ class ContentLocationExtractor
   def initialize(doc)
     @doc = doc
     @key_metadata = {}
-    @current_exam_paper_version = ''
     @ns = ''
     @headers = []
   end
@@ -31,11 +30,24 @@ class ContentLocationExtractor
     @headers
   end
 
+  # in some cases the binary documents, notably of thumbnails but perhaps others
+  # too have different versions, so it is neccesary to confirm which are current
+  # - the Active (A) flag is not sufficient as does not seem to get changed
+  def get_current_ds_version(cid)
+    nums = @doc.xpath("//foxml:datastream[@ID='#{cid}']/"\
+      "foxml:datastreamVersion/@ID", @ns)
+    all = nums.to_s
+    current = all.rpartition('.').last
+    current_ds_version = cid.to_s + '.' + current.to_s
+    current_ds_version
+  end
+
   def extract_resource_locations
     #  get all  ids for paths with content locations then extract the ids
     # then run the paths for just the ids to get the content locations
     content_ids = []
     # get all the ids of ds with contentLocation
+    # returns  id of every content ds. Content datastreams can be versioned
     path = "//foxml:datastream[@STATE='A']/foxml:datastreamVersion"\
     "/foxml:contentLocation/../../@ID"
     @doc.xpath(path, @ns).each do |id|
@@ -43,9 +55,11 @@ class ContentLocationExtractor
     end
 
     # now use them to construct a path to get the ref
+    # this is where we need the datastream version
     content_ids.each do |cid|
+      current_version = get_current_ds_version(cid.to_s)
       path2 = "//foxml:datastream[@STATE='A'][@ID='#{cid}']"\
-      "/foxml:datastreamVersion/foxml:contentLocation/@REF"
+            "/foxml:datastreamVersion[@ID='#{current_version}']/foxml:contentLocation/@REF"
       content_location = @doc.xpath(path2, @ns).to_s
       @key_metadata[cid] = content_location
     end
