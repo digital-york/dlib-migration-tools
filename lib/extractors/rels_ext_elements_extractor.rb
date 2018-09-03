67762b82f8c2b5a80988ee2c94635725005b55ab
straccers <ps552@york.ac.uk>
2018-09-03 12:55:30 +0100

    refactor extractors and downcase headers
    move extractors into separate subfolder and downcase rel-ext and acl 
    headers

--- lib/extractors/rels_ext_elements_extractor.rb
+++ lib/extractors/rels_ext_elements_extractor.rb
@@ -7,29 +7,30 @@ class RelsExtElementsExtractor
     @key_metadata = {}
     @current_rels_ext_version = ''
     @ns = ''
-    @headers = ['isMemberOf']
+    @headers = ['ismemberof']
   end
 
-  # this will do the meat of extracting the metadata values and putting them into a hash
+  # extracts metadata values
   def extract_key_metadata
-    @ns = @doc.collect_namespaces #  nokogiri out of box doesnt resolve nested namespaces,
-    # this fixes that
+    # fix for nokogiri out of box not resolving nested namespaces
+    @ns = @doc.collect_namespaces
     # estabish current  version to use before extracting any further values
     determine_current_rels_ext_version
     # extract rest of key metadata
-    extract_single_valued_element('rel', 'isMemberOf') # immediate parent coll
+    extract_single_valued_element('rel', 'isMemberOf') # immediate parent
     # possible relevance, not sure if needed
-    #extract_multivalued_element('fedora-model', 'hasModel')
+    # extract_multivalued_element('fedora-model', 'hasModel')
     @key_metadata
   end
 
   def collect_headers
-    @ns = @doc.collect_namespaces
-    determine_current_rels_ext_version
-    multi_value_elements = %w[hasModel]
-    multi_value_elements.each do |mve|
-      extract_multivalued_element_headers('fedora-model', mve)
-    end
+    # think its is unlikely we will need the hasModel elements
+    # @ns = @doc.collect_namespaces
+    # determine_current_rels_ext_version
+    # multi_value_elements = %w[hasModel]
+    # multi_value_elements.each do |mve|
+    #   extract_multivalued_element_headers('fedora-model', mve)
+    # end
     @headers
   end
 
@@ -40,7 +41,7 @@ class RelsExtElementsExtractor
     "rdf:Description/#{element_prefix}:#{element_name}/@rdf:resource"
     @doc.xpath(path, @ns).each do
       i += 1
-      @headers.push(element_name + i.to_s)
+      @headers.push(element_name.downcase + i.to_s)
     end
   end
 
@@ -54,6 +55,7 @@ class RelsExtElementsExtractor
 
   # generic method to  values where value is an element which may
   # occur  0:n times in a single rels-ext version
+  # have removed hasModel element at present so may no longer need
   def extract_multivalued_element(element_prefix, element_name)
     i = 0
     path = "//foxml:datastream[@ID='RELS-EXT']/foxml:datastreamVersion"\
@@ -61,7 +63,7 @@ class RelsExtElementsExtractor
     "rdf:Description/#{element_prefix}:#{element_name}/@rdf:resource"
     @doc.xpath(path, @ns).each do |s|
       i += 1
-      keyname = element_name
+      keyname = element_name.downcase
       keyname += i.to_s
       keyname = keyname.to_sym
       @key_metadata[keyname] = s.to_s
@@ -76,7 +78,8 @@ class RelsExtElementsExtractor
     "rdf:Description/#{element_prefix}:#{element_name}/@rdf:resource"
     element = @doc.xpath(path, @ns).to_s
     return if element.empty?
+    element_name = element_name.downcase
     keyname = element_name.to_sym
-    @key_metadata[keyname] = element
+    @key_metadata[keyname] = element.to_s
   end
 end
