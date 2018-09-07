1d07fda0d13412bedb0d9acc73dff199af319d83
straccers <ps552@york.ac.uk>
2018-09-07 12:08:01 +0100

    methods to normalise department names
    at present these do not actually normalise, just identify those elements 
    containing dapertment names and pass them to a cleaning method which 
    returns a placefiller string

---
+++ lib/cleaners/department_cleaner.rb
@@ -0,0 +1,16 @@
+#!/usr/bin/env ruby
+require 'English'
+
+# class to put department values into agreed standard form
+class DepartmentCleaner
+  def standardise_department(name)
+    # this will need to handle multiple values from several possible sources
+    # is it possible to use a wildcard in the #key? method?
+    # https://www.safaribooksonline.com/library/view/ruby-cookbook/0596523696/ch05s15.html
+    puts 'this will return initial names '
+  end
+
+  def clean_name(name)
+    puts 'mickey mouse'
+  end
+end
--- lib/cleaners/metadata_cleaner.rb
+++ lib/cleaners/metadata_cleaner.rb
@@ -1,10 +1,12 @@
 #!/usr/bin/env ruby
 require_relative 'date_cleaner.rb'
+require_relative 'department_cleaner'
 
 # class to co-ordinate data cleaning
 class MetadataCleaner
   def clean_metadata(key_metadata)
     clean_date(key_metadata)
+    clean_department_names(key_metadata)
   end
 
   def clean_date(key_metadata)
@@ -15,5 +17,27 @@ class MetadataCleaner
       key_metadata[:date] = clean_date unless clean_date.to_s.empty?
     end
     key_metadata
+  end
+
+  def clean_department_names(key_metadata)
+    dept_keys_array = get_department_keys(key_metadata)
+    dept_cleaner = DepartmentCleaner.new unless dept_keys_array.empty?
+    dept_keys_array.each do |k|
+      name = key_metadata.fetch(k)
+      standard_name = dept_cleaner.clean_name(name)
+      puts 'standard_name ' + standard_name
+      key_metadata[k] = standard_name unless standard_name.to_s.empty?
+    end
+    key_metadata
+  end
+
+  def get_department_keys(key_metadata)
+    # identify keys for both possible sets of sources (creatorX,publisherX)
+    # return as array
+    # https://www.safaribooksonline.com/library/view/ruby-cookbook/0596523696/ch05s15.html
+    creators_hash = key_metadata.select { |k, _| k.to_s.include? 'creator' }
+    publishers_hash = key_metadata.select { |k, _| k.to_s.include? 'publisher' }
+    depts_hash = creators_hash.merge(publishers_hash)
+    depts_hash.keys
   end
 end
