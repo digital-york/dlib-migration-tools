f470c07035ef54d08f5e331338a0157e50211d90
straccers <ps552@york.ac.uk>
2018-09-18 10:50:15 +0100

    add name matching for batchelor level qualifications
    fleshing out skeleton methods in qualification_name_cleaner.rb

--- lib/cleaners/qualification_name_cleaner.rb
+++ lib/cleaners/qualification_name_cleaner.rb
@@ -4,20 +4,30 @@
 class QualificationNameCleaner
 
   # a lot of variation so need to refer against arrays of possible variants
+  # TODO: add other exams, check against additional docs
   def initialize
   # bachelors
   @medicine_surgery_bachelors = ['bachelor of medicine',
                                  'bachelor of surgery (mbbs)',
-                                 'bachelor of medicine', 'bachelor of surgery',
+                                 'bachelor of surgery',
                                  'mbbs']
   @med_sci_bachelors = ['bachelor of medical science (bmedsci)',
                         'bachelor of medical science','bmedsci']
   @science_bachelors = ['batchelor of science (bsc)',
                         'bachelor of science (bsc)',
                         '"bachelor of science (bsc)"', 'bsc',
-                        'bachelor of science (ba)','bachelor of science (bsc )',
+                        'bachelor of science (ba)',
+                        'bachelor of science (bsc )',
                         'bachelor of science', 'bachelor of science (bsc)',
                         'bachelor of science (msc)']
+  @art_bachelors = ['batchelor of arts (ba)', '"bachelor of arts (ba),"', 'ba',
+                    'bachelor of arts (ba)', 'bachelor of art (ba)',
+                    'bachelor of arts', 'bachelor of arts (ma)']
+  @philosophy_bachelors = ['bachelor of philosophy (bphil)',
+                           'bachelor of philosophy', 'bphil']
+  @engineering_bachelors = ['bachelor of engineering (beng)',
+                            'bachelor of engineering', 'beng']
+  @law_bachelors = ['bachelor of laws (llb)', 'bachelor of laws', 'llb']
 
   # TODO populate  other arrays later
   end
@@ -28,6 +38,8 @@ class QualificationNameCleaner
     name
   end
 
+  # TODO bachelors now populated according to old code, need to check latest docs
+  # TODO other xams not yet added
   def get_standard_name(name)
     if @medicine_surgery_bachelors.include? name
       standard_name = 'Bachelor of Medicine, Bachelor of Surgery (MBBS)'
@@ -35,6 +47,14 @@ class QualificationNameCleaner
       standard_name = 'Bachelor of Medical Science (BMedSci)'
     elsif @science_bachelors.include? name
       standard_name = 'Bachelor of Science (BSc)'
+    elsif @art_bachelors.include? name
+      standard_name = 'Bachelor of Arts (Ba)'
+    elsif @philosophy_bachelors.include? name
+      standard_name = 'Bachelor of Philosophy (BPhil)'
+    elsif @engineering_bachelors.include? name
+      standard_name = 'Bachelor of Engineering (BEng)'
+    elsif @law_bachelors.include? name
+      standard_name = 'Bachelor of Laws (LLB)'
     else
       standard_name = 'COULD NOT MATCH ' + name
     end
