0cedafac296070c614528a89656291e1df1add9c
straccers <ps552@york.ac.uk>
2018-09-27 09:52:52 +0100

    refactoring

--- lib/cleaners/department_cleaner.rb
+++ lib/cleaners/department_cleaner.rb
@@ -13,7 +13,8 @@ class DepartmentCleaner
     name = name.downcase
     case name
     when /reconstruction/
-      standard_name = 'University of York. Post-war Reconstruction and Development Unit'
+      standard_name = 'University of York. Post-war Reconstruction and
+       Development Unit'
     when /applied human rights/
       standard_name = 'University of York. Centre for Applied Human Rights'
     when /health economics/
@@ -25,29 +26,32 @@ class DepartmentCleaner
     when /medieval studies/
       standard_name = 'University of York. Centre for Medieval Studies'
     when /renaissance/
-      standard_name = 'University of York. Centre for Renaissance and Early Modern Studies'
+      standard_name = 'University of York. Centre for Renaissance and Early
+       Modern Studies'
     when /reviews/
-      standard_name = 'University of York. Centre for Reviews and Disseminations'
+      standard_name = 'University of York. Centre for Reviews and
+       Disseminations'
     when /women/
       standard_name = "University of York. Centre for Women's Studies"
     when /languages for all/
       standard_name = 'University of York. Languages for All'
     when /school of social and political science/
-      standard_name = 'University of York. School of Social and Political Science'
+      standard_name = 'University of York. School of Social and Political
+       Science'
     when /social policy/
-      standard_name =  'University of York. Department of Social Policy and Social Work'
+      standard_name = 'University of York. Department of Social Policy and
+       Social Work'
     when /school of politics economics and philosophy/
-      standard_name = 'University of York. School of Politics Economics and Philosophy'
+      standard_name = 'University of York. School of Politics Economics and
+       Philosophy'
     when /politics/
       standard_name = 'University of York. Department of Politics'
     when /economics and related/
-      standard_name = 'University of York. Department of Economics and Related Studies'
+      standard_name = 'University of York. Department of Economics and Related
+       Studies'
     when /economics and philosophy/
-      standard_name = 'University of York. School of Politics Economics and Philosophy'
-      # elsif name.include? 'departments of english and history of art'   # damn! two departments to return! MUST precede history of art
-      # name =  'University of York. Department of English and Related Literature'
-      # name = 'University of York. Department of History of Art'
-      # at top so looks for history later. but below english and history of art!
+      standard_name = 'University of York. School of Politics Economics and
+       Philosophy'
     when /history of art/
       standard_name = 'University of York. Department of History of Art'
     when /history/
@@ -55,7 +59,8 @@ class DepartmentCleaner
     when /electronic/
       standard_name = 'University of York. Department of Electronic Engineering'
     when /theatre/
-      standard_name = 'University of York. Department of Theatre, Film and Television'
+      standard_name = 'University of York. Department of Theatre, Film and
+       Television'
     when /physics/
       standard_name = 'University of York. Department of Physics'
     when /computer/
@@ -67,11 +72,13 @@ class DepartmentCleaner
     when /mathematics/
       standard_name = 'University of York. Department of Mathematics'
     when /advanced architectural/
-      standard_name = 'University of York. Institute of Advanced Architectural Studies'
+      standard_name = 'University of York. Institute of Advanced Architectural
+       Studies'
     when /conservation/
       standard_name = 'University of York. Centre for Conservation Studies'
     when /eighteenth century/
-      standard_name = 'University of York. Centre for Eighteenth Century Studies'
+      standard_name = 'University of York. Centre for Eighteenth Century
+       Studies'
     when /chemistry/
       standard_name = 'University of York. Department of Chemistry'
     when /sociology/
@@ -87,7 +94,8 @@ class DepartmentCleaner
     when /biochemistry/ # confirmed with metadata team - recheck?
       standard_name =  'University of York. Department of Biology'
     when /english and related literature/
-      standard_name =  'University of York. Department of English and Related Literature'
+      standard_name =  'University of York. Department of English and Related
+       Literature'
     when /philosophy/
       standard_name =  'University of York. Department of Philosophy'
     when /management studies/
@@ -96,9 +104,11 @@ class DepartmentCleaner
       # older versionof department name which should be retained if match found
       standard_name =  'University of York. The York Management School'
     when /language and linguistic science/
-      standard_name = 'University of York. Department of Language and Linguistic Science'
+      standard_name = 'University of York. Department of Language and
+       Linguistic Science'
     when /language and lingusitic science/ # deal with common typo
-      standard_name = 'University of York. Department of Language and Linguistic Science' 
+      standard_name = 'University of York. Department of Language and
+       Linguistic Science'
     when /hull/
       standard_name = 'Hull York Medical School'
     when /international pathway/
