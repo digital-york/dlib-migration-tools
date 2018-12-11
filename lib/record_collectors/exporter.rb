5a6a45e18047c9693262d338276db6e293c5279b
straccers <ps552@york.ac.uk>
2018-12-11 15:47:11 +0000

    added export logging

--- lib/record_collectors/exporter.rb
+++ lib/record_collectors/exporter.rb
@@ -7,12 +7,16 @@ class Exporter
 
   # run the export command on a fedora server for records identified in pid_file
   # then export to local machine ready for metadata extraction
+  # rake metadata_extraction_tasks:export_records['yodlapp3.york.ac.uk',
+  # <digilib_pwd>,<fedora_pwd>,<pidfile (without file extension)>,
+  # <destination on local box>]
   def export_foxml(host, digilib_pwd, fed_password, pid_file, to_dir)
     puts 'emptying directory'
     ensure_empty_export_dir(host, digilib_pwd)
     puts 'ensured empty /tmp/fedora_exports dir, preparing to export records'
-    bindir = '/opt/york/digilib/fedora/client/bin'
+    bin_dir = '/opt/york/digilib/fedora/client/bin'
     export_dir = '/tmp/fedora_exports'
+    log_dir = './logs'
     feduser = 'fedoraAdmin'
     fedhost = host.shellescape
     digilib_pwd = digilib_pwd.shellescape
@@ -24,10 +28,13 @@ class Exporter
 
     Net::SSH.start(fedhost, main_user, :password => digilib_pwd) do|ssh|
       args = "#{feduser} #{fed_password} #{fedhost} #{export_dir} #{pidlist}"
-      res = ssh.exec!("cd #{bindir} && './exportRecordBatch.sh' #{args}")
+      res = ssh.exec!("cd #{bin_dir} && './exportRecordBatch.sh' #{args}")
       puts 'res ' + res # worth leaving this as it gives any error output
-    end
-    exec("sshpass -p #{digilib_pwd} sftp #{main_user}@#{fedhost}:#{export_dir}/*.xml #{to_dir}")
+    end  
+    ftp_records = "sshpass -p #{digilib_pwd} sftp #{main_user}@#{fedhost}:#{export_dir}/*.xml #{to_dir}"
+    ftp_logfile = "sshpass -p #{digilib_pwd} sftp #{main_user}@#{fedhost}:#{bin_dir}/export.log #{log_dir}"
+    system("#{ftp_records}")
+    system("#{ftp_logfile}")
   end
 
   def ensure_empty_export_dir(host, digilib_pwd)
